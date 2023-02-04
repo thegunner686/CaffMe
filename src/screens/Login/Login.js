9b36e180c414eb5407d125b39a3a6c12019bafda
Gunner Spencer <ngspence@ucsc.edu>
2023-02-04 14:39:48 -0800

    react-navigation and dev dependencies

---
+++ src/screens/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const LoginScreen = () => {
+  return <SafeAreaView><Text>Login</Text></SafeAreaView>
+}
+
+export default LoginScreen;
