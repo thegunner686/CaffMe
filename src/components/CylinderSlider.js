bd7c38ac0a5ce2c67528205f58aa0453955e4a2a
Gunner Spencer <ngspence@ucsc.edu>
2023-02-04 18:20:36 -0800

    slider animation

---
+++ src/components/CylinderSlider.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  useDerivedValue,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
+import { TouchableOpacity, Text, View, Image } from 'react-native';
+
+const CylinderSlider = ({ level }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (level < 33) {
+      setIndex(0);
+    } else if (level < 67) {
+      setIndex(1);
+    } else {
+      setIndex(2);
+    }
+    console.log({ level });
+  }, [level]);
+
+  const height = useDerivedValue(() => {
+    return level / 100;
+  }, [level]);
+
+  const imageStyle = useAnimatedStyle(() => {
+    return { transform: [{ scale: withSpring(height.value) }], width: withSpring(level * 3) };
+  }, [level]);
+
+  const images = [
+    require('../../assets/Latte.png'),
+    require('../../assets/Coffee.png'),
+    require('../../assets/Yerb.png'),
+  ];
+
+  return (
+    <Animated.Image
+      source={images[index]}
+      style={[{ resizeMode: 'contain', alignSelf: 'center' }, imageStyle]}
+    />
+  );
+};
+
+export default CylinderSlider;
--- src/screens/AddCaffeine/AddCaffeine.js
+++ src/screens/AddCaffeine/AddCaffeine.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import CylinderSlider from '../../components/CylinderSlider';
+import VerticalSlider from 'rn-vertical-slider';
 
 const AddCaffeineScreen = () => {
+  const [level, setLevel] = useState(0);
+  const [caffeine, setCaffeine] = useState(0);
+  const [color, setColor] = useState('#FDFFFC');
+
+  useEffect(() => {
+    let max = 500;
+    let caff = max * (level / 100);
+    setCaffeine(caff);
+
+    if (caff < 200) {
+      setColor('#FDFFFC');
+    } else if (caff < 300) {
+      setColor('#FFE156');
+    } else {
+      console.log(' set color ');
+      setColor('#D66853');
+    }
+  }, [level, setColor]);
+
   return (
-    <SafeAreaView className="flex flex-1 bg-space-cadet">
-      <Text>Add Caffeine</Text>
+    <SafeAreaView className="flex flex-1 flex-col items-center justify-end bg-space-cadet">
+      <Text className="self-center pt-4 text-5xl font-bold text-baby-powder" style={{ color }}>
+        {caffeine}mg
+      </Text>
+      <View className="flex w-full flex-grow flex-row items-center">
+        <View className="h-auto w-9/12">
+          <CylinderSlider level={level} />
+        </View>
+        <View className="flex-shrink p-2">
+          <VerticalSlider
+            value={level}
+            disabled={false}
+            min={0}
+            max={100}
+            onChange={setLevel}
+            onComplete={setLevel}
+            width={50}
+            height={300}
+            step={1}
+            borderRadius={5}
+            minimumTrackTintColor={'#FDFFFC'}
+            maximumTrackTintColor={'#1E202E'}
+          />
+        </View>
+      </View>
+      <View className="w-full flex-1 items-center">
+        <TouchableOpacity className="flex h-12 w-7/12 flex-col items-center justify-center rounded bg-ocean-green">
+          <Text className="text-center text-lg font-bold text-baby-powder">Done</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
